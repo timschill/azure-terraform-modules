c8d75dbb667705fe582b4e6b45a0606e18e7d3b3
Tim Schill <35256819+timschill@users.noreply.github.com>
2024-01-11 14:57:49 +0100

    Fixed snake case

--- storage-container/outputs.tf
+++ storage-container/outputs.tf
@@ -1,4 +1,4 @@
 output "az_sa_container_id" {
-  value       = azurerm_storage_container.az-sa-container.id
+  value       = azurerm_storage_container.az_sa_container.id
   description = "Storage container ID"
 }
